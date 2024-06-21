f687a9764c1bf365c5f8ab9c9d40d28c9c10ff29
Maia <matteusmaia889@gmail.com>
2024-06-21 03:17:00 -0300

    feat: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-export default clerkMiddleware();
+import { authMiddleware } from "@clerk/nextjs/server";
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
