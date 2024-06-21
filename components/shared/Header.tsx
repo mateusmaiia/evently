a607adf88b55bab4ee6179b763412f7828b90cd9
Maia <matteusmaia889@gmail.com>
2024-06-21 03:19:40 -0300

    feat: Header with auth by Clerk

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,5 +1,9 @@
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "../ui/button"
+import { NavItems } from "./NavItem"
+import { MobileNav } from "./MobileNav"
 
 export const Header = () => {
   return (
@@ -9,7 +13,25 @@ export const Header = () => {
           <Image alt="Evently Logo" src="/assets/images/logo.svg" width={128} height={38}/>
         </Link>
 
-        <div className="flex w-32 justify-end gap-3"></div>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn >
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">
+                Login
+              </Link>
+            </Button>
+          </SignedOut>
+        </div>
       </div>
     </header>
   )
