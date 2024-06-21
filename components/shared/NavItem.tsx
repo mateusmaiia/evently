da3e58a87b38f1b1a07f332659a8badc15fb7d16
Maia <matteusmaia889@gmail.com>
2024-06-21 03:19:01 -0300

    feat: NavItems and MobileNav

--- components/shared/NavItem.tsx
+++ components/shared/NavItem.tsx
@@ -1,14 +1,33 @@
+'use client';
+
 import { headerLinks } from "@/constants";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function NavItems( ){
+  
+  const pathname = usePathname()
+
   return(
     <ul className="md:flex-between flex w-full flex-col md:flex-row items-start gap-5 ">
       {headerLinks.map(link => {
+
+      const isActive = pathname === link.route 
+
         return(
-          <Link href={link.route}>
-            {link.label}
-          </Link>
+          <li 
+            key={link.route}
+            className={`
+              ${isActive && 'text-primary-500' }
+              flex-center p-medium-16 whitespace-nowrap
+            `}
+          >
+            <Link 
+              href={link.route}
+            >
+              {link.label}
+            </Link>
+          </li>
         )
       })}
     </ul>   
