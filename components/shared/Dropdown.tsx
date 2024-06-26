d5b52ab5e4c4b3bad49c67125522de87dcb3fdc0
Maia <matteusmaia889@gmail.com>
2024-06-26 13:06:52 -0300

    feat: Dropdown

---
+++ components/shared/Dropdown.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { ICategory } from "@/lib/database/models/category.model";
+import { startTransition, useState } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Input } from "../ui/input";
+
+
+interface DropdownProps {
+  onChangeHandle?: () => void;
+  value?: string;
+}
+
+const Dropdown = ({onChangeHandle, value}: DropdownProps) => {
+  const [categories, setCategories] = useState<ICategory[]>([])
+  const [newCategory, setNewCategory] = useState('')
+
+  function handleAddCategory(){
+  return console.log("ain")
+  }
+
+  return (
+    <Select onValueChange={onChangeHandle} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 && categories.map((category) => {
+          return (
+            <SelectItem 
+              key={category._id} 
+              value={category.name}
+              className="select-item p-regular-14"
+            >
+              {category.name}
+            </SelectItem>
+          )
+        })}
+        <AlertDialog>
+        <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Open</AlertDialogTrigger>
+        <AlertDialogContent className="bg-white">
+          <AlertDialogHeader>
+            <AlertDialogTitle>New category</AlertDialogTitle>
+            <AlertDialogDescription>
+              <Input type="text" placeholder="New category" className="input-field mt-3" onChange={(e) => setNewCategory(e.target.value)}/>
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction onClick={() => startTransition(handleAddCategory)}>Add</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      </SelectContent>
+
+      
+    </Select>
+
+  )
+}
+
+export default Dropdown
